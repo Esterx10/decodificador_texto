54721434ee3660e60c3d0beb889956c48a454537
Ester Carvalho <esterdesousa625@gmail.com>
2024-04-20 17:10:45 -0300

    projeto inicial

---
+++ interacaoJS/script.js
@@ -0,0 +1,62 @@
+const textoPrincipal = document.querySelector(".texto_principal");
+const caixaDireita = document.querySelector(".caixa_direita");
+document.querySelector(".bt-copiar").addEventListener("click", copiarResultado);
+
+
+
+function btcriptar() {
+    const textEncriptado = criptar(textoPrincipal.value);
+    caixaDireita.innerHTML =  `<p>${textEncriptado}</p>`;
+    textoPrincipal.value = "";
+    botaoCopiar();
+}
+
+function criptar(textoCriptado) {
+
+    let codigo = [['e', 'enter'], ['i', 'imes'], ['a', 'ai'], ['o', 'ober'], ['u', 'ufat']];
+    textoCriptado = textoCriptado.toLowerCase();
+
+
+    for (let i = 0; i < codigo.length; i++) {
+        if(textoCriptado.includes(codigo[i] [0])) {
+            textoCriptado = textoCriptado.replaceAll(codigo[i] [0], codigo[i] [1]);
+        }
+    }
+
+    return textoCriptado;
+}
+
+function btDescriptar() {
+    const textDesencriptado = descriptar(textoPrincipal.value);
+    caixaDireita.innerHTML =  `<p>${textDesencriptado}</p>`;
+    textoPrincipal.value = "";
+    botaoCopiar();
+}
+
+function descriptar(textoDescriptado) {
+
+    let codigo = [['e', 'enter'], ['i', 'imes'], ['a', 'ai'], ['o', 'ober'], ['u', 'ufat']];
+    textoDescriptado = textoDescriptado.toLowerCase();
+
+
+    for (let i = 0; i < codigo.length; i++) {
+        if(textoDescriptado.includes(codigo[i] [1])) {
+            textoDescriptado = textoDescriptado.replaceAll(codigo[i] [1], codigo[i] [0]);
+        }
+    }
+
+    return textoDescriptado;
+}
+
+function botaoCopiar() {
+    const botaoCopiar = document.createElement("button");
+    botaoCopiar.textContent = "Copiar";
+    botaoCopiar.className = "bt-copiar";
+    botaoCopiar.addEventListener("click", () => {
+        const resultado = caixaDireita.querySelector("p").innerText;
+        navigator.clipboard.writeText(resultado)
+            .then(() => alert('Resultado copiado!'));
+    });
+
+    caixaDireita.appendChild(botaoCopiar);
+}
